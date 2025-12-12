79e07015fcb35178e6708f98b7ff198bc1fd17a3
avb24025 <avb24022005@gmail.com>
2025-12-12 20:41:38 +0530

    Updated Port

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ dotenv.config();
 
 const app = express();
 
+const PORT = process.env.PORT || 5000;
+
 app.use(cors());
 app.use(express.json());
 
@@ -17,7 +19,6 @@ mongoose.connect(process.env.MONGO_URI)
 
 app.use("/api", pixelRoutes);
 // app.use("/", pixelRoutes);   // For /track routes
-
-app.listen(5000, () => {
-    console.log("Server running on port 5000");
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
